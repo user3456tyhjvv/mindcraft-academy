0d06064832f4407fb3f38aff8ab10a085053335b
sammymuigai131 <34409512-sammymuigai131@users.noreply.replit.com>
2025-06-03 08:52:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// let titleDiv = document.getElementById('title');
+// console.log('before:', titleDiv.innerText);
+// let messege = 'Goodbye my lover';
+// titleDiv.innerText = messege;
+// console.log('after:', titleDiv.innerText)
+
+// titleDiv.innerText = `<p>${message}</p>`
+// titleDiv.style.backgroundColor = 'red';
+
+
+let yellowDiv = document.getElementById('yellow');
+let greenDiv = document.getElementById('green');
+redDiv.onclick = () => console.log('red');
